9a11bad0978521f5440b7a6e90d6131a7de71947
Tongzhou-Yu <tongzhou.yu@network.rca.ac.uk>
2022-02-02 14:22:52 +0800

    WebAR added

--- Projects WIP/Hydra_Synth_Snowflakes/sketch.js
+++ Projects WIP/Hydra_Synth_Snowflakes/sketch.js
@@ -1,7 +1,10 @@
- var hydra = new Hydra({
+const hydra = new Hydra({
         canvas: document
          .getElementById("myCanvas"),
-        detectAudio: false
+         width: window.width,
+         height: window.height,
+        detectAudio: false,
+        precision: 'highp'
       })
        osc(40,0.1,0.6)
          .modulateScale(osc(40,0,1).kaleid(6))
